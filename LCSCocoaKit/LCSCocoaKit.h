86119d7c33d68b92a419c519c171c9fe339b98c1
Christoph Lauterbach <christoph@MacBook-Pro.local>
2014-08-12 11:25:52 +0200

    Initial Commit

---
+++ LCSCocoaKit/LCSCocoaKit.h
@@ -0,0 +1,13 @@
+//
+//  LCSCocoaKit.h
+//  LCSCocoaKit
+//
+//  Created by Christoph Lauterbach on 12.08.14.
+//  Copyright (c) 2014 Christoph Lauterbach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LCSCocoaKit : NSObject
+
+@end
---
+++ LCSCocoaKit/LCSCocoaKit.m
@@ -0,0 +1,13 @@
+//
+//  LCSCocoaKit.m
+//  LCSCocoaKit
+//
+//  Created by Christoph Lauterbach on 12.08.14.
+//  Copyright (c) 2014 Christoph Lauterbach. All rights reserved.
+//
+
+#import "LCSCocoaKit.h"
+
+@implementation LCSCocoaKit
+
+@end
